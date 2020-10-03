3b096d7672cf98a1a1a786e4a55519a09d4d4e4c
Tulle12 <72298643+Tulle12@users.noreply.github.com>
2020-10-03 13:45:11 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+function cwU () {
+    music.setTempo(400)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.setTempo(133)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+}
+input.onButtonPressed(Button.A, function () {
+    cwU()
+})
 basic.forever(function () {
     basic.showString("D")
     basic.pause(500)
